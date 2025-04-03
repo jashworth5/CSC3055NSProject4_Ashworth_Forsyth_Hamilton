e56e163051e99b36ca01aa860f1bb67bcdb18633
David <hamiltondm@merrimack.edu>
2025-04-03 18:49:21 -0400

    Load users from users.json if file exists

--- src/server/UserDatabase.java
+++ src/server/UserDatabase.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import merrimackutil.json.JsonIO;
+import merrimackutil.json.types.JSONArray;
 import merrimackutil.json.types.JSONObject;
 
 public class UserDatabase {
@@ -13,6 +15,23 @@ public class UserDatabase {
     public UserDatabase(String filePath) {
         this.file = new File(filePath);
         this.users = new HashMap<>();
+
+        if (file.exists()) {
+            try {
+                JSONObject root = JsonIO.readObject(file);
+                JSONArray userArray = (JSONArray) root.get("users");
+
+                for (int i = 0; i < userArray.size(); i++) {
+                    JSONObject userObj = (JSONObject) userArray.get(i);
+                    String username = (String) userObj.get("user");
+                    users.put(username, userObj);
+                }
+
+                System.out.println("[+] Loaded " + users.size() + " user(s) from database.");
+            } catch (Exception e) {
+                System.err.println("[!] Failed to load user database: " + e.getMessage());
+            }
+        }
     }
 
     public boolean userExists(String username) {
